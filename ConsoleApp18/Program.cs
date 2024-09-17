fbf4046d8d1b78941c85019201efd9ec5f1e5b1d
РИШАТ Ахтямов <rishat-rus@mail.ru>
2024-09-17 13:38:41 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,28 @@
+﻿// track
+Console.WriteLine("список команд");
+Console.WriteLine("Команда 1 : \"+\"");
+Console.WriteLine("Команда 2 : \"-\"");
+Console.WriteLine("Команда 3 : \"test\"");
+Console.WriteLine("Команда выход : \"exit\"");
+
+
+
+while (true)
+{
+    Console.WriteLine("Введите команды:");
+
+
+    switch (Console.ReadLine())
+    {
+        case "+": Console.WriteLine("начинаю команду 1 ..."); break;
+        case "-": Console.WriteLine("начинаю команду 2 ..."); break;
+        case "test": Console.WriteLine("начинаю команду тест"); break;
+        case "exit": return;  //Выход
+
+        default:
+            Console.WriteLine("неизветная команда");
+            break;
+    }
+
+}
+
